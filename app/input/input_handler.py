b41c4eccab94d099f7d248249cd9991e7b191dab
charlesmmarshall <charles.marshall@digital.justice.gov.uk>
2021-07-12 13:05:34 +0100

    move to a proper class for io handling

---
+++ app/input/input_handler.py
@@ -0,0 +1,69 @@
+import argparse
+import json
+
+class input_handler:
+    """
+    Class to handler all the input variables and parsing passed to the main command.
+    """
+    args: argparse.Namespace
+    arg_parser: argparse.ArgumentParser
+    arg_parser_description: str = 'Generate cost table for specificed aws accounts.'
+
+    def parser(self):
+        """
+        Construct the argument parser object with all arguments
+        """
+        self.arg_parser = argparse.ArgumentParser(description=self.arg_parser_description)
+        self.arg_parser.add_argument(
+                    '--aws-access-key-id',
+                    help='AWS access key id property for auth',
+                    required=True
+                    )
+        self.arg_parser.add_argument(
+                    '--aws-secret-access-key',
+                    help='AWS secret for auth',
+                    required=True
+                    )
+        self.arg_parser.add_argument(
+                    '--aws-region',
+                    help='AWS region',
+                    default='eu-west-1',
+                    )
+        self.arg_parser.add_argument(
+                    '--months',
+                    default=6,
+                    type=int,
+                    help='Number of months ago to start report from.',
+                    )
+        self.arg_parser.add_argument(
+                    '--accounts',
+                    default='[]',
+                    type=json.loads,
+                    help='JSON array of objects containing all account data',
+                    )
+        return self
+
+    def parse(self):
+        """
+        Parse & validate the input arguments.
+
+        --accounts must have at least one entry and that must have an arn & label
+        --months must be between 1 & 12 due to cost explorer limitations
+        """
+        args = self.arg_parser.parse_args()
+
+        if len(args.accounts) < 1:
+            raise Exception('input_handler', 'Account input parameter invalid')
+        else:
+            filtered = list(filter(lambda acc: 'arn' in acc and 'label' in acc, args.accounts))
+            if len(filtered) != len(args.accounts):
+                raise Exception('input_handler', 'One of the accounts passed does not contain both arn & label attributes.')
+
+        if args.months < 1:
+            raise Exception('input_handler', 'Month counter has to be between 1 and 12 (found < 1)')
+        elif args.months > 12:
+            raise Exception('input_handler', 'Month counter has to be between 1 and 12 (found > 12)')
+
+        # no errors, so assign to self
+        self.args = args
+        return self
--- app/inputHandling/accounts.py
+++
@@ -1,7 +0,0 @@
-import json
-
-def accounts(raw: str):
-    parsed = json.loads(raw)
-    if len(parsed) < 1:
-        raise Exception('Account Data', 'Account data missing, requires at least 1 account.')
-    return parsed
