1f361639a0b3f23d68ce1f91521a3c2a82ebf091
charlesmmarshall <charles.marshall@digital.justice.gov.uk>
2021-07-12 13:06:24 +0100

    wip on main

--- app/main.py
+++ app/main.py
@@ -3,18 +3,20 @@
 import sys
 import pprint
 
-from inputHandling.parser import parser
-from inputHandling.validate import validate
-from inputHandling.accounts import accounts
+from input.input_handler import input_handler
+from aws.costs import costs
 
 pp = pprint.PrettyPrinter(indent=4)
 
 def main():
-    args = parser().parse_args()
-    validate(args)
-    account = accounts(args.accounts)
+    io = input_handler()
+    args = io.parser().parse().args
 
-    pp.pprint(account[0]['arn'])
+    for account in args.accounts:
+        pp.pprint(account)
+        cost = costs(account['arn'], args.aws_region, account['label'])
+        data = cost.auth().get(args.months)
+        pp.pprint(data)
 
 
 
