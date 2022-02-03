367d399adf9da9118a25899f73d6c359fdf30751
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2022-02-03 10:57:25 +0000

    remove unused headers variable

--- lambda/costs-to-metrics/send/send.py
+++ lambda/costs-to-metrics/send/send.py
@@ -10,11 +10,9 @@ def chunks(lst, number):
     """
     Yield successive n-sized chunks from list (lst).
     """
-    for i in enumerate(lst):
-        yield lst[i:i + number]
 
-    # for i in range(0, len(lst), number):
-    #     yield lst[i:i + number]
+    for i in range(0, len(lst), number):
+        yield lst[i:i + number]
 
 
 def send(results: list, chunksize: int = 1):
@@ -24,13 +22,12 @@ def send(results: list, chunksize: int = 1):
     """
     length = len(results)
     chunked = list(chunks(results, chunksize))
-    headers = {'x-api-key': 'XXX',
-               'Content-Type': 'application/json; charset=utf-8'}
 
     logger.info(
         "Sending total of %s metrics in %s chunks", length, len(chunked))
+
     for i in range(len(chunked)):
-        logger.info(f"Chunk {i}")
+        logger.info("Chunk %s", i)
         data = chunked[i]
         body = {'metrics': data}
         sqs_send_message(body)
