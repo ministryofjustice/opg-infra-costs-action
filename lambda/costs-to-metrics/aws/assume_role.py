5b5525ca90321aa7aa4a00e8144d0c0672f0c0ba
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2022-07-12 13:45:25 +0100

    add new class to start and end an assume role session

---
+++ lambda/costs-to-metrics/aws/assume_role.py
@@ -0,0 +1,23 @@
+import boto3
+import logging
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+
+class AssumeRole:
+
+    def __init__(self):
+        self.client = boto3.client('sts')
+
+    def start_session(self, role_arn: str):
+        assumed_role = self.client.assume_role(
+            RoleArn=role_arn,
+            RoleSessionName='opg-costs-to-metrics-lambda',
+            DurationSeconds=123,
+        )
+        logger.info(assumed_role)
+        return assumed_role
+
+    def close_session(self):
+        self.client.close()
